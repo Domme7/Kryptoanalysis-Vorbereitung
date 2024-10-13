81348ef0d1196a9c9808aebe72136f10a8fe9abb
domme7 <dominik.hu@outlook.de>
2024-10-13 12:16:35 +0200

    konverter datei erstellt

---
+++ konverter.py
@@ -0,0 +1,9 @@
+dezimal_zahl = 42
+
+
+binär = bin(dezimal_zahl)
+hexadezimal = hex(dezimal_zahl)
+
+print(f"Dezimalzahl: {type(dezimal_zahl)}")
+print(f"Binärzahl: {binär[2:]}")
+print(f"Hexadezimal: {type(hexadezimal[2:])}")
