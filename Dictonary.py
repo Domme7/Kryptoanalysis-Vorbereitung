0a1491faa043b3b137fbebe51ee6989baa58c0a8
domme7 <dominik.hu@outlook.de>
2024-10-13 11:52:43 +0200

    Dictonary datei erstellt

---
+++ Dictonary.py
@@ -0,0 +1,8 @@
+buch = {
+    "Titel": "Python lernen leicht gemacht",
+    "Author": "Max Musterman",
+    "Veröffentlichungsjahr": 2021,
+    "ISBN": 123-456-789
+}
+
+print(f"Der Titel: {buch['Titel']} und den Author: {buch['Author']}")
